2f6be10d8481988d1f11de145f6a737c17393f5f
Stefano Pica <stefanopica@Mac-mini-di-Stefano.local>
2022-05-29 23:26:04 +0200

    add

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Image from "next/image";
 import ButtonPrimary from "./misc/ButtonPrimary";
+
+import ReactPlayer from 'react-player/youtube'
+
 const Hero = ({
   listUser = [
     {
@@ -22,30 +25,99 @@ const Hero = ({
 }) => {
   return (
     <div className="max-w-screen-xl mt-24 px-8 xl:px-16 mx-auto" id="about">
+      
+
+<h1 className="font-serif italic text-5xl text-center	lg:text-6xl xl:text-7xl font-medium text-orange-500 leading-normal py-10">
+            <strong><u>EndLess Road</u></strong>.
+          </h1>
+
+       
+          
+
       <div className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16 ">
         <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
           <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
-            Want anything to be easy with <strong>LaslesVPN</strong>.
+            In Cammino Adesso <strong>BASTA</strong>.
           </h1>
           <p className="text-black-500 mt-4 mb-6">
-            Provide a network for all your needs with ease and fun using
-            LaslesVPN discover interesting features from us.
+            Scivere del testo qui ---- Scivere del testo qui
+            Scivere del testo qui ---- Scivere del testo qui  
+            Scivere del testo qui.
           </p>
           <ButtonPrimary>Get Started</ButtonPrimary>
         </div>
         <div className="flex w-full">
           <div className="h-full w-full">
-            <Image
-              src="/assets/Illustration1.png"
-              alt="VPN Illustrasi"
-              quality={100}
-              width={612}
-              height={383}
-              layout="responsive"
-            />
+         
+                <div className='player-wrapper'>
+                    <ReactPlayer url='https://www.youtube.com/watch?v=GlYZ9pD-2Fw' 
+                        playing= {true}
+                      //muted={true}
+                        className='react-player'
+                        width='100%'
+                        height='100%'
+
+                    />
+
+              </div>
           </div>
         </div>
       </div>
+
+
+                   {/* aggiunta      */ } 
+
+
+      <div className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16 ">
+
+          <div className="flex w-full">
+              <div className="h-full w-full">
+            
+                    <div className='player-wrapper'>
+                        <ReactPlayer url='https://www.youtube.com/watch?v=6qnaEA8piJs' 
+                            playing= {true}
+                          //muted={true}
+                            className='react-player'
+                            width='100%'
+                            height='100%'
+
+                        />
+
+
+                              {/*<Image
+                                src="/assets/Illustration1.png"
+                                alt="VPN Illustrasi"
+                                quality={100}
+                                width={612}
+                                height={383}
+                                layout="responsive"
+                                />*/}
+
+                  </div>
+              </div>
+            </div>
+
+
+            <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1 ml-5">
+              <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
+              Parola al Coronavirus <strong>(ALTALENA)</strong>.
+              </h1>
+              <p className="text-black-500 mt-4 mb-6">
+              Scivere del testo qui ---- Scivere del testo qui
+              Scivere del testo qui ---- Scivere del testo qui  
+              Scivere del testo qui.
+              </p>
+              <ButtonPrimary>Get Started</ButtonPrimary>
+            </div>
+        
+      </div>
+
+
+
+                  {/* fine aggiunta */}
+
+      
+
       <div className="relative w-full flex">
         <div className="rounded-lg w-full grid grid-flow-row sm:grid-flow-row grid-cols-1 sm:grid-cols-3 py-9 divide-y-2 sm:divide-y-0 sm:divide-x-2 divide-gray-100 bg-white-500 z-10">
           {listUser.map((listUsers, index) => (

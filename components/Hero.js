e3ce3de0f7be414272ae22502b64362a106cf71d
Stefano Pica <stefanopica@Mac-mini-di-Stefano.local>
2022-06-02 21:01:14 +0200

    modified:   components/Hero.js modified:   public/sw.js.map

--- components/Hero.js
+++ components/Hero.js
@@ -26,9 +26,9 @@ const Hero = ({
     <div className="max-w-screen-xl mt-24 px-8 xl:px-16 mx-auto" id="about">
       
 
-<h1 className="font-serif italic text-5xl text-center	lg:text-6xl xl:text-7xl font-medium text-orange-500 leading-normal py-10">
-            <strong><u>EndLess Road</u></strong>.
-          </h1>
+      <h1 className="font-serif italic text-5xl text-center	lg:text-6xl xl:text-7xl font-medium text-orange-500 leading-normal py-10">
+        <strong><u>EndLess Road</u></strong>.
+      </h1>
 
        
           
@@ -40,50 +40,50 @@ const Hero = ({
           </h1>
           <p className="text-black-500 mt-4 mb-6">
             Scivere del testo qui ---- Scivere del testo qui
-            Scivere del testo qui ---- Scivere del testo qui  
+            Scivere del testo qui ---- Scivere del testo qui
             Scivere del testo qui.
           </p>
           <ButtonPrimary>Get Started</ButtonPrimary>
         </div>
         <div className="flex w-full">
           <div className="h-full w-full">
-         
-                <div className='player-wrapper'>
-                    <ReactPlayer url='https://www.youtube.com/watch?v=GlYZ9pD-2Fw' 
-                        playing= {true}
-                      //muted={true}
-                        className='react-player'
-                        width='100%'
-                        height='100%'
 
-                    />
+            <div className='player-wrapper'>
+              <ReactPlayer url='https://www.youtube.com/watch?v=GlYZ9pD-2Fw'
+                playing={true}
+                //muted={true}
+                className='react-player'
+                width='100%'
+                height='100%'
 
-              </div>
+              />
+
+            </div>
           </div>
         </div>
       </div>
 
 
-                   {/* aggiunta      */ } 
+      {/* aggiunta      */}
 
 
       <div className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16 ">
 
-          <div className="flex w-full">
-              <div className="h-full w-full">
-            
-                    <div className='player-wrapper'>
-                        <ReactPlayer url='https://www.youtube.com/watch?v=6qnaEA8piJs' 
-                            playing= {true}
-                          //muted={true}
-                            className='react-player'
-                            width='100%'
-                            height='100%'
+        <div className="flex w-full">
+          <div className="h-full w-full">
 
-                        />
+            <div className='player-wrapper'>
+              <ReactPlayer url='https://www.youtube.com/watch?v=6qnaEA8piJs'
+                playing={true}
+                //muted={true}
+                className='react-player'
+                width='100%'
+                height='100%'
+
+              />
 
 
-                              {/*<Image
+              {/*<Image
                                 src="/assets/Illustration1.png"
                                 alt="VPN Illustrasi"
                                 quality={100}
@@ -92,30 +92,30 @@ const Hero = ({
                                 layout="responsive"
                                 />*/}
 
-                  </div>
-              </div>
             </div>
+          </div>
+        </div>
 
 
-            <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1 ml-5">
-              <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
-              Parola al Coronavirus <strong>(ALTALENA)</strong>.
-              </h1>
-              <p className="text-black-500 mt-4 mb-6">
-              Scivere del testo qui ---- Scivere del testo qui
-              Scivere del testo qui ---- Scivere del testo qui  
-              Scivere del testo qui.
-              </p>
-              <ButtonPrimary>Get Started</ButtonPrimary>
-            </div>
-        
+        <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1 ml-5">
+          <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
+            Parola al Coronavirus <strong>(ALTALENA)</strong>.
+          </h1>
+          <p className="text-black-500 mt-4 mb-6">
+            Scivere del testo qui ---- Scivere del testo qui
+            Scivere del testo qui ---- Scivere del testo qui
+            Scivere del testo qui.
+          </p>
+          <ButtonPrimary>Get Started</ButtonPrimary>
+        </div>
+
       </div>
 
 
 
-                  {/* fine aggiunta */}
+      {/* fine aggiunta */}
 
-      
+
 
       <div className="relative w-full flex">
         <div className="rounded-lg w-full grid grid-flow-row sm:grid-flow-row grid-cols-1 sm:grid-cols-3 py-9 divide-y-2 sm:divide-y-0 sm:divide-x-2 divide-gray-100 bg-white-500 z-10">

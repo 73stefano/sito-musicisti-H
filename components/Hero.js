6259550782c78d4dd09a10fdc530e6ef149b4a89
Stefano Pica <stefanopica@Mac-mini-di-Stefano.local>
2022-06-02 20:06:27 +0200

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     Committer: Stefano Pica <stefanopica@Mac-mini-di-Stefano.local>
    
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   components/Hero.js
    	modified:   package-lock.json
    	modified:   package.json
    	deleted:    test.js
    	modified:   yarn.lock
    
     Untracked files:
    	next.config.js
    	public/sw.js
    	public/sw.js.ma
    	public/workbox-74d02f44.js
    	public/workbox-74d02f44.js.map
    ok
    
    Ωqq

--- components/Hero.js
+++ components/Hero.js
@@ -1,8 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import ButtonPrimary from "./misc/ButtonPrimary";
-
-import ReactPlayer from 'react-player/youtube'
+import ReactPlayer from 'react-player/lazy';
 
 const Hero = ({
   listUser = [
--- test.js
+++
@@ -1,9 +0,0 @@
-<iframe
-          id="ytplayer"
-          className={}
-          type="text/html"
-          width="100%"
-          height="360"
-          src={`https://www.youtube.com/embed/${videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
-          frameborder="0">
-            </iframe>

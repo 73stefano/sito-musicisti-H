6b6e572d50cdb3439b73b7701ccb950bf1d85a4c
Stefano Pica <stefanopica@Mac-mini-di-Stefano.local>
2022-06-06 01:00:07 +0200

    ok

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -23,7 +23,7 @@ const Header = () => {
       >
         <nav className="bg-rose-50 max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col py-3 sm:py-4">
           <div className="col-start-1 col-end-2 flex items-center">
-            <LogoVPN className="h-12 lg:h-20 w-auto" />
+            <LogoVPN className="h-12 lg:h-20 md:h-20 w-auto" />
           </div>
           <ul className="hidden lg:flex col-start-4 col-end-8 text-black-500  items-center">
             <LinkScroll
